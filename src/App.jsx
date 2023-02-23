9d6ad062bf10573449fff0abc5b6baff149386ca
Dmytro Chernetskyi <chernetskyi.private@gmail.com>
2023-02-23 11:02:30 +0200

    Added the necessary files and reset styles.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import './styles/App.css'
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Notes from "./pages/Notes";
+import "./styles/App.css";
 
 const App = () => {
   return (
     <div className="App">
-      <h1>React App</h1>
+      <h1>App</h1>
     </div>
-  )
-}
+  );
+};
 
 export default App;
