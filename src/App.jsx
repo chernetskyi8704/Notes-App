b5ea721432cd9d4ebb04a346ec803558cf826952
Dmytro Chernetskyi <chernetskyi.private@gmail.com>
2023-02-23 12:51:19 +0200

    Have created a global authentication context.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
+import React from "react";
 import AppRouter from "./components/AppRouter";
 import Navigation from "./components/UI/Navigation";
+import { AuthContext } from "./context/AuthContext";
 import "./styles/App.css";
 
 const App = () => {
+  const [isAuth, setIsAuth] = React.useState(false);
+
   return (
-    <div className="App">
-      <Navigation />
-      <main className="mainContent">
-        <AppRouter />
-      </main>
-    </div>
+    <AuthContext.Provider value={{ isAuth, setIsAuth }}>
+      <div className="App">
+        <Navigation />
+        <main className="mainContent">
+          <AppRouter />
+        </main>
+      </div>
+    </AuthContext.Provider>
   );
 };
 
