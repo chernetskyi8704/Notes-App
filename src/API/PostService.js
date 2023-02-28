07a2ad403f36a21bbe9c20dde442b830f3d6e346
Dmytro Chernetskyi <chernetskyi.private@gmail.com>
2023-02-28 12:13:21 +0200

    Transferred the necessary data for displaying projects to a separate json file. I created and used a separate component that will be responsible for requests for data retrieval.

---
+++ src/API/PostService.js
@@ -0,0 +1,15 @@
+export default class PostService {
+  static async getProjects() {
+    const response = await fetch("/src/data/projects.json");
+    const data = await response.json();
+    const projects = data.map(project => {
+      return {
+        name: project.name,
+        link: project.link,
+        image: project.image,
+        alt: project.name,
+      };
+    });
+    return projects;
+  }
+}
