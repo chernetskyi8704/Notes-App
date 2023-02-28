6d06d925468efc3827eadda139ba2cdb720cbd85
Dmytro Chernetskyi <chernetskyi.private@gmail.com>
2023-02-28 14:42:52 +0200

    Used lazy initialization to optimize our app.

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -7,59 +7,63 @@ import { nanoid } from "nanoid";
 import ModalWindow from "../components/UI/modalWindow/ModalWindow";
 
 const Notes = () => {
-  const [notes, setNotes] = React.useState(
-    JSON.parse(localStorage.getItem("notes")) || [
-      {
-        id: nanoid(),
-        title: "Note 1",
-        description: "Description 1",
-        date: "2021-01-01",
-        color: "#6E9ECF",
-      },
-      {
-        id: nanoid(),
-        title: "Note 2",
-        description: "Description 2",
-        date: "2021-01-01",
-        color: "#9ACD32",
-      },
-      {
-        id: nanoid(),
-        title: "Note 3",
-        description: "Description 3",
-        date: "2021-01-01",
-        color: "#F49AC2",
-      },
-      {
-        id: nanoid(),
-        title: "Note 4",
-        description: "Description 4",
-        date: "2021-01-01",
-        color: "#BAA8D8",
-      },
-      {
-        id: nanoid(),
-        title: "Note 5",
-        description: "Description 5",
-        date: "2021-01-01",
-        color: "#D2B48C",
-      },
-    ]
-  );
+  const [notes, setNotes] = React.useState(() => {
+    return (
+      JSON.parse(localStorage.getItem("notes")) || [
+        {
+          id: nanoid(),
+          title: "Note 1",
+          description: "Description 1",
+          date: "2021-01-01",
+          color: "#6E9ECF",
+        },
+        {
+          id: nanoid(),
+          title: "Note 2",
+          description: "Description 2",
+          date: "2021-01-01",
+          color: "#9ACD32",
+        },
+        {
+          id: nanoid(),
+          title: "Note 3",
+          description: "Description 3",
+          date: "2021-01-01",
+          color: "#F49AC2",
+        },
+        {
+          id: nanoid(),
+          title: "Note 4",
+          description: "Description 4",
+          date: "2021-01-01",
+          color: "#BAA8D8",
+        },
+        {
+          id: nanoid(),
+          title: "Note 5",
+          description: "Description 5",
+          date: "2021-01-01",
+          color: "#D2B48C",
+        },
+      ]
+    );
+  });
 
   React.useEffect(() => {
     localStorage.setItem("notes", JSON.stringify(notes));
   }, [notes]);
 
-  const [notesSettings, setNotesSettings] = React.useState({
-    isModal: false,
-    isEdit: false,
-    currentNote: {},
-    isAddNew: false,
-    color: "",
-    currentData: new Date().toLocaleDateString(),
-    title: "",
-    description: "",
+  const [notesSettings, setNotesSettings] = React.useState(() => {
+    return {
+      isModal: false,
+      isEdit: false,
+      currentNote: {},
+      isAddNew: false,
+      color: "",
+      currentData: new Date().toLocaleDateString(),
+      title: "",
+      description: "",
+    };
   });
 
   React.useEffect(() => {
