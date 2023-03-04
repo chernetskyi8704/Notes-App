3764e473b0f62e60bd00b91c357c925b89ce604e
Dmytro Chernetskyi <chernetskyi.private@gmail.com>
2023-03-04 13:58:49 +0200

    Added animations for adding and removing notes using react-transition-group.

--- src/components/NotesItems.jsx
+++ src/components/NotesItems.jsx
@@ -1,20 +1,27 @@
 import React from "react";
 import NoteItem from "./NoteItem";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import "../styles/animations/NotesItemsAnimations.css";
 
 const NotesItems = ({ notes, setNotes, setNotesSettings, notesSettings }) => {
-  return notesSettings.searchNotes.map(note => (
-    <NoteItem
-      key={note.id}
-      title={note.title}
-      body={note.description}
-      bodyColour={note.color}
-      currentData={note.date}
-      setNotes={setNotes}
-      notes={notes}
-      id={note.id}
-      setNotesSettings={setNotesSettings}
-    />
-  ));
+  return (
+    <TransitionGroup component={null}>
+      {notesSettings.searchNotes.map(note => (
+        <CSSTransition timeout={500} key={note.id} classNames="note">
+          <NoteItem
+            title={note.title}
+            body={note.description}
+            bodyColour={note.color}
+            currentData={note.date}
+            setNotes={setNotes}
+            notes={notes}
+            id={note.id}
+            setNotesSettings={setNotesSettings}
+          />
+        </CSSTransition>
+      ))};
+    </TransitionGroup>
+  );
 };
 
 export default NotesItems;
