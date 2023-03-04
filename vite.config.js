2d69cc0d07c6ea35ad9996d20824ad1ad2429b00
Dmytro Chernetskyi <chernetskyi.private@gmail.com>
2023-03-04 22:07:23 +0200

    Fixed the API request.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Notes-App/',
-})
+  base: "/Notes-App/",
+  server: {
+    middleware: [
+      createProxyMiddleware("/api", {
+        target: "https://api.npoint.io/330476ee1cf37edbfd70",
+        changeOrigin: true,
+      }),
+    ],
+  },
+});
