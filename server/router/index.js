5a606bd4fa33c83961bab725949f20c74d34f6ca
Dmytro Chernetskyi <chernetskyi.private@gmail.com>
2023-04-27 12:36:28 +0300

    Removed unnecessary comments.

--- server/router/index.js
+++ server/router/index.js
@@ -2,12 +2,11 @@ const Router = require("express").Router;
 const userController = require("../controllers/user-controller");
 const notesController = require("../controllers/notes-controller");
 const router = new Router();
-const { body } = require("express-validator"); 
+const { body } = require("express-validator");
 const authMiddleware = require("../middlewares/auth-middleware.js");
 
 router.post(
   "/registration",
-  // =========================================
   body("firstName")
     .notEmpty()
     .withMessage("This field is required!")
@@ -24,9 +23,7 @@ router.post(
       max: 50,
     })
     .withMessage("Your last name should consist at least 2 symbols."),
-  // =========================================
-  body("email").notEmpty().isEmail().withMessage("Check the entered email."), //4 Цю функцію ми передаємо в якості middleware в нашому запиті, та передаємо назву поля яке ми хочемо провалідувати
-  // =========================================
+  body("email").notEmpty().isEmail().withMessage("Check the entered email."),
   body("password").isLength({
     min: 3,
     max: 32,
@@ -34,9 +31,9 @@ router.post(
   userController.registration
 );
 router.post("/login", userController.login);
-router.post("/logout", userController.logout); 
-router.get("/activate/:link", userController.activate); 
-router.get("/refresh", userController.refresh); 
+router.post("/logout", userController.logout);
+router.get("/activate/:link", userController.activate);
+router.get("/refresh", userController.refresh);
 router.get("/notes/:id", authMiddleware, notesController.getAllNotes);
 router.post("/createNote", authMiddleware, notesController.createNote);
 router.put("/update/:id", authMiddleware, notesController.updateNote);
