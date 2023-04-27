d6483ae7e513596aa7493e433bafae4abe17f13e
Dmytro Chernetskyi <chernetskyi.private@gmail.com>
2023-04-27 15:56:13 +0300

    Updated CORS settings.

--- server/index.js
+++ server/index.js
@@ -7,17 +7,17 @@ const mongoose = require("mongoose");
 const router = require("./router/index.js");
 const errorMiddleware = require("./middlewares/error-middleware.js");
 const app = express();
-const corsOptions = require("./config/corsOptions.js");
+// const corsOptions = require("./config/corsOptions.js");
 
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors(corsOptions));
+app.use(cors());
 app.use("/api", router);
 app.use(errorMiddleware);
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://notes-app-chernetskyi.vercel.app');
+  res.header("Access-Control-Allow-Origin", "https://notes-app-chernetskyi.vercel.app");
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   res.header('Access-Control-Allow-Credentials', true);
