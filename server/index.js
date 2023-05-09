7f5362a206f03b2d4828c217ac476261c16c1abb
Dmytro Chernetskyi <chernetskyi.private@gmail.com>
2023-05-09 12:23:06 +0300

    Replace http import with require for Node.js.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
-import http from "http";
 
+const http = require( "http");
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
